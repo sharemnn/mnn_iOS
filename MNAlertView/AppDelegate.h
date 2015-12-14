a0393420f3f5bfc69613383ca7410dba335734a4
mnn <mnn@mnndemac-mini.lan>
2015-12-14 09:40:05 +0800

    Initial Commit

---
+++ MNAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MNAlertView
+//
+//  Created by mnn on 15/12/14.
+//  Copyright © 2015年 mnn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MNAlertView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MNAlertView
+//
+//  Created by mnn on 15/12/14.
+//  Copyright © 2015年 mnn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
